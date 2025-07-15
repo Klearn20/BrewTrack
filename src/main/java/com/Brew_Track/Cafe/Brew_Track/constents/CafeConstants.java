2c95b2bcdc2c23443cd94c9b3753aadc265fd461
Klearn20 <59652329+blackknigtt@users.noreply.github.com>
2025-07-15 09:05:26 +0800

    Update CafeConstants.java

--- src/main/java/com/Brew_Track/Cafe/Brew_Track/constents/CafeConstants.java
+++ src/main/java/com/Brew_Track/Cafe/Brew_Track/constents/CafeConstants.java
@@ -1,7 +1,7 @@
 package com.Brew_Track.Cafe.Brew_Track.constents;
 
 public class CafeConstants {
-  
+ss  
 	public static final String SOMETHING_WENT_WRONG= "Something Went Wrong.";
 	
 	public static final String INVALID_DATA = "Invalid Data.";
